8a17632de3df7373cdc8aee2d0b9c81d06f6c50a
penCsharpener <matthiasmueller@protonmail.com>
2019-08-13 11:33:58 +0200

    S06L13: learning about global middleware

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-
 app.use(express.json());
+
+// global route handler middleware are before route handlers
+app.use((req, res, next) => {
+  console.log('Hello from the middleware.');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -12,6 +22,7 @@ const tours = JSON.parse(
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours
@@ -87,12 +98,6 @@ const deleteTour = (req, res) => {
     data: null
   });
 };
-
-app.get('/api/v1/tours', getAllTours);
-app.get('/api/v1/tours/:id', getTour);
-app.post('/api/v1/tours', createTour);
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
 
 app
   .route('/api/v1/tours')
