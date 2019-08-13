6783df156e694b93657fd29afc9c6aee9e6e546e
penCsharpener <matthiasmueller@protonmail.com>
2019-08-13 19:14:34 +0200

    S06L20: serving static files

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 // global route handler middleware are before route handlers
 app.use((req, res, next) => {
@@ -22,12 +23,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// 1) ROUTE HANDLERS
-
-// 3) ROUTES
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-// 4) START SERVER
 
 module.exports = app;
