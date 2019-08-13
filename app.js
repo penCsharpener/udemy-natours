8d9dbc7c403be6af7007418f0cb2a31b047ae4c3
penCsharpener <matthiasmueller@protonmail.com>
2019-08-13 11:25:50 +0200

    S06L10-2: use app.route()... chaining

--- app.js
+++ app.js
@@ -94,6 +94,17 @@ app.post('/api/v1/tours', createTour);
 app.patch('/api/v1/tours/:id', updateTour);
 app.delete('/api/v1/tours/:id', deleteTour);
 
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
 const port = 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
