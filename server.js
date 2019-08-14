4f5750d2c8c3c5da15d60eaf17e3c4c0f867d4a9
penCsharpener <matthiasmueller@protonmail.com>
2019-08-14 12:51:51 +0200

    S08L02-2: connect to local mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then((/*con*/) => {
+    //console.log(con.connections);
+    console.log('DB connection successful!');
+  });
 
 // console.log(app.get('env'));
 // console.log(process.env);
