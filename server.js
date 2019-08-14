b8db9d846fce1a1669d4c2700f4c51e2b69ca669
penCsharpener <matthiasmueller@protonmail.com>
2019-08-14 12:55:54 +0200

    S08L04: create mongoose model for tours

--- server.js
+++ server.js
@@ -20,6 +20,22 @@ mongoose
 // console.log(app.get('env'));
 // console.log(process.env);
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
