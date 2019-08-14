f056056fe3b4e12525f0418bf92a58e7013ca817
penCsharpener <matthiasmueller@protonmail.com>
2019-08-14 13:59:40 +0200

    S08L08: recreate create tour route with mongodb

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,18 +1,8 @@
-//const Tour = require('./../models/tourModel');
+const Tour = require('./../models/tourModel');
 
 // exports.tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price.'
-    });
-  }
-  next();
-};
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -38,13 +28,22 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success'
-    // data: {
-    //   tour: newTour
-    // }
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!'
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
