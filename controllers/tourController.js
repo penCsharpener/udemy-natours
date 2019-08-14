013ab363d29992d89f54fa7abaee254b209f8f4d
penCsharpener <matthiasmueller@protonmail.com>
2019-08-14 13:49:27 +0200

    S08L07: remove code referring to fs based data

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,21 +1,8 @@
-const fs = require('fs');
+//const Tour = require('./../models/tourModel');
 
-exports.tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  const id = val * 1;
-  const tour = this.tours.find(el => el.id === id);
-  console.log(`params middleware id: ${id}`);
-  if (id > this.tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-  next();
-};
+// exports.tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -30,44 +17,34 @@ exports.checkBody = (req, res, next) => {
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTime,
-    results: this.tours.length,
-    data: {
-      tours: this.tours
-    }
+    requestedAt: req.requestTime
+    // results: this.tours.length,
+    // data: {
+    //   tours: this.tours
+    // }
   });
 };
 
 exports.getTour = (req, res) => {
   console.log(req.params);
-  const id = req.params.id * 1;
-  const tour = this.tours.find(el => el.id === id);
+  // const id = req.params.id * 1;
+  // const tour = this.tours.find(el => el.id === id);
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour
+  //   }
+  // });
 };
 
 exports.createTour = (req, res) => {
-  console.log(req.body);
-  const newId = this.tours[this.tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  this.tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(this.tours),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success'
+    // data: {
+    //   tour: newTour
+    // }
+  });
 };
 
 exports.updateTour = (req, res) => {
