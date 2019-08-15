09699436b4b1bb90ceaa06e7fb4eba70f2cbd4f1
penCsharpener <matthiasmueller@protonmail.com>
2019-08-15 17:33:24 +0200

    S08L10: implement update tour method

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -59,13 +59,25 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: 'Updated tour...'
-    }
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findOneAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!'
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
