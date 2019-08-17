b2b7e5244f103ca0007cde6c072e11f20d75d126
penCsharpener <matthiasmueller@protonmail.com>
2019-08-17 20:20:37 +0200

    S10L07: user login route, hiding password hash from api

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,24 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
+const User = require('./../models/userModel');
+const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const features = new APIFeatures(User.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const users = await features.query;
+
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: users.length,
+    data: {
+      users
+    }
   });
-};
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
