f5abbfbe82c8e7a3c3a4cbd32bab7aab69a9b15a
penCsharpener <matthiasmueller@protonmail.com>
2019-08-17 13:59:38 +0200

    improve import-dev-data.js script with better async await

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -7,59 +7,65 @@ dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE_LOCAL;
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(() => {
-    console.log('DB connection successful!');
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
   });
-
-// READ JSON FILE
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-// );
-
-// IMPORT DATA INTO DB
-const importData = () => {
-  fs.readFile(
-    `${__dirname}/tours-simple.json`,
-    'utf-8',
-    async (errFS, data) => {
-      try {
-        if (errFS) {
-          throw errFS;
-        }
-        const tours = JSON.parse(data);
-        console.log(`${tours.length} tours found in file to import.`);
-        await Tour.create(tours);
-        console.log('Data successfully loaded!');
-      } catch (err) {
-        console.log(err);
-      }
-      process.exit();
-    }
-  );
 };
 
-// DELETE DATA FROM MONGO COLLECTION
-const deleteData = async () => {
+// IMPORT DATA INTO DB
+const importData = async () => {
   try {
-    await Tour.deleteMany();
-    console.log('Data successfully deleted!');
+    const dataPath = `${__dirname}/tours-simple.json`;
+    console.log('importing file: ' + dataPath);
+
+    const data = await readFilePro(dataPath);
+    const tours = JSON.parse(data);
+    console.log(`${tours.length} tours found in file to import.`);
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
   }
   process.exit();
 };
 
-if (process.argv[2] === '--import') {
-  importData();
-} else if (process.argv[2] === '--delete') {
-  deleteData();
-} else if (process.argv[2] === '--refresh') {
-  deleteData();
-  importData();
-}
+// DELETE DATA FROM MONGO COLLECTION
+const deleteData = async importFn => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  if (importFn) {
+    await importFn();
+  }
+  process.exit();
+};
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(async () => {
+    console.log('DB connection successful!');
+
+    if (process.argv[2] === '--import') {
+      await importData();
+    } else if (process.argv[2] === '--delete') {
+      await deleteData();
+    } else if (process.argv[2] === '--refresh') {
+      await deleteData(importData);
+    }
+  });
+
+// READ JSON FILE
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+// );
