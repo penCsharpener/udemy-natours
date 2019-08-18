9d05d037b4de8f8e374343a4230aa72bc0a05ee7
penCsharpener <matthiasmueller@protonmail.com>
2019-08-18 19:19:10 +0200

    S10L12: add password forgot route

--- controllers/authController.js
+++ controllers/authController.js
@@ -104,3 +104,18 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with email address.', 404));
+  }
+
+  // 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+  // 3) Send it to the user's email
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {});
