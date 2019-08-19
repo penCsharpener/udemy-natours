4928d826a57dea635923a5e5090168e8200c6108
penCsharpener <matthiasmueller@protonmail.com>
2019-08-19 12:46:28 +0200

    fix bug with false usage of promisify

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,7 +68,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     );
   }
   // 2) Verification token
-  const decoded = await promisify(jwt.verify(token, process.env.JWT_SECRET));
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // 3) Check if user still exists
   const freshUser = await User.findById(decoded.id);
