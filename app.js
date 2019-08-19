c5e04857c48cf7d8347ec196fbdab41f35954cbf
penCsharpener <matthiasmueller@protonmail.com>
2019-08-19 20:00:35 +0200

    S10L21: add more http security headers with helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,12 +10,16 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
 
+// development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 3600 * 1000,
@@ -23,10 +28,13 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
-// global route handler middleware are before route handlers
+// test middleware
 app.use((req, res, next) => {
   console.log('Hello from the middleware.');
   next();
