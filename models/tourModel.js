7fd9f4e044c470d05289b991ce8338d7cfd85420
penCsharpener <matthiasmueller@protonmail.com>
2019-08-20 08:44:09 +0200

    S11L05: modelling tour guides: embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
+
 //const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -103,7 +105,8 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -118,6 +121,11 @@ tourSchema.virtual('durationWeeks').get(function() {
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
+});
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
 });
 
 tourSchema.post('save', function(doc, next) {
