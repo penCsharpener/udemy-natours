79e10f527e4b43c4f75e1f0abb70062cd6d6a8e5
penCsharpener <matthiasmueller@protonmail.com>
2019-08-21 12:30:41 +0200

    S11L13: nested routes with express

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-const router = express.Router();
+// mergeParams allows for passing through the tourId from the tour Router
+const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
