e39b3327b5aaf66033e447869db39514619e926e
penCsharpener <matthiasmueller@protonmail.com>
2019-08-21 13:09:32 +0200

    S11L16: building handler factory functions: update/create

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -14,3 +14,34 @@ exports.deleteOne = Model =>
       data: null
     });
   });
+
+exports.createOne = Model =>
+  catchAsync(async (req, res) => {
+    const newDoc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: newDoc
+      }
+    });
+  });
+
+exports.updateOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
--- controllers/userController.js
+++ controllers/userController.js
@@ -69,12 +69,7 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-};
+exports.createUser = factory.createOne(User);
 
 exports.getUser = (req, res) => {
   res.status(500).json({
@@ -83,11 +78,7 @@ exports.getUser = (req, res) => {
   });
 };
 
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-};
+// do not update passwords with this!
+exports.updateUser = factory.updateOne(User);
 
 exports.deleteUser = factory.deleteOne(User);
