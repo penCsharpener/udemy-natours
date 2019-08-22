851af017439591f0f146f8dd684215e8a933d15f
penCsharpener <matthiasmueller@protonmail.com>
2019-08-22 06:11:31 +0200

    S11L20: importing users and reviews

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModel');
+const User = require('./../../models/userModel');
+const Review = require('./../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -19,13 +21,28 @@ const readFilePro = file => {
 // IMPORT DATA INTO DB
 const importData = async () => {
   try {
-    const dataPath = `${__dirname}/tours.json`;
-    console.log('importing file: ' + dataPath);
+    /*
+    const tourDataPath = `${__dirname}/tours.json`;
+    console.log(`importing file: ${tourDataPath}`);
 
-    const data = await readFilePro(dataPath);
+    const data = await readFilePro(tourDataPath);
     const tours = JSON.parse(data);
     console.log(`${tours.length} tours found in file to import.`);
     await Tour.create(tours);
+
+    to import users comment out the two pre save middlewares in userModel.js
+    const userDataPath = `${__dirname}/users.json`;
+    const userData = await readFilePro(userDataPath);
+    const users = JSON.parse(userData);
+    console.log(`${users.length} users found in file to import.`);
+    await User.create(users, { validateBeforeSave: false });
+    */
+
+    const reviewDataPath = `${__dirname}/reviews.json`;
+    const reviewData = await readFilePro(reviewDataPath);
+    const reviews = JSON.parse(reviewData);
+    await Review.create(reviews);
+
     console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
@@ -36,7 +53,9 @@ const importData = async () => {
 // DELETE DATA FROM MONGO COLLECTION
 const deleteData = async importFn => {
   try {
-    await Tour.deleteMany();
+    // await Tour.deleteMany();
+    // await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted!');
   } catch (err) {
     console.log(err);
