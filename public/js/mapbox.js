29f7b017570b4bc18848e53cc4e5f2d05b780a2f
penCsharpener <matthiasmueller@protonmail.com>
2019-08-26 06:52:06 +0200

    S12L14: including a map with mapbox pt 2

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,48 @@
 /* eslint-disable */
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoicGVuY3NoYXJwZW5lciIsImEiOiJjanpyeDA5c2kxOTlkM2xyMTU0aXhzdDY1In0.Sgs6hzRDxE9kESJFP31sxA';
+var map = new mapboxgl.Map({
+  // container is an html element with the id #map or whatever you specify here
+  container: 'map',
+  style: 'mapbox://styles/pencsharpener/cjzrvaoii3ab61co0dv8pimbo',
+  scrollZoom: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  // create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom'
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // add popup
+  new mapboxgl.Popup({
+    offset: 40
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100
+  }
+});
