aa1467fb66727aa7ea9594e7847795c48af38b4e
penCsharpener <matthiasmueller@protonmail.com>
2019-08-26 07:36:08 +0200

    S12L16: logging in users with api - pt1

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookieParser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -41,6 +42,7 @@ app.use('/api', limiter);
 
 // body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -64,12 +66,8 @@ app.use(
 
 // test middleware
 app.use((req, res, next) => {
-  console.log('Hello from the middleware.');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
---
+++ public/js/login.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+
+const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://localhost:8000/api/v1/users/login',
+      data: {
+        email,
+        password
+      }
+    });
+  } catch (err) {
+    console.log(err.response.data);
+  }
+};
+
+document.querySelector('.form').addEventListener('submit', e => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  login(email, password);
+});
